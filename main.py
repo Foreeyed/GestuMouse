7a7f6922257ffd44c8dec8b1d1bf4edddb62d5c9
Four-eyed <arstybe@gmail.com>
2026-09-09 15:46:04 +0300

    version 3:the program shows centre  the objects with max area in ROI (Region of interest)

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ lower_red2 = np.array([160, 100, 100])
 upper_red2 = np.array([179, 255, 255])
 
 
+
 if camera.isOpened():
     while camera.isOpened():
         success, frame = camera.read()
@@ -29,33 +30,52 @@ if camera.isOpened():
             full_mask_roi = full_mask_frame[y1:y2, x1:x2]
 
             contours, _ = cv2.findContours(full_mask_roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            #cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+            max_cnt = None
+            max_area = 0
+
             for i, cnt in enumerate(contours):
                 area = cv2.contourArea(cnt)
-                M = cv2.moments(cnt)
+                #M = cv2.moments(cnt)
 
-                # Опционально: можно отфильтровать слишком мелкий шум
-                if area > 100 and M["m00"] != 0:
-                    local_cX = int(M["m10"] / M["m00"])
-                    local_cY = int(M["m01"] / M["m00"])
+                if area > max_area:
+                    max_area = area
+                    max_cnt = cnt
 
-                    x, y, w, h = cv2.boundingRect(cnt)
-                    global_x = x + x1
-                    global_y = y + y1
-
-                    global_cX = local_cX + x1
-                    global_cY = local_cY + y1
 
                     #print(f"Контур №{i + 1}: Площадь = {area} пикселей, X:{global_x} Y:{global_y}, Width:{w} Height:{h}")
-                    cv2.rectangle(frame, (global_x, global_y), (global_x + w, global_y + h), (0, 255, 0), 3)
-                    cv2.circle(frame, (global_cX, global_cY), 5, (0, 255, 0), -1)
+                    # cv2.rectangle(frame, (global_x, global_y), (global_x + w, global_y + h), (0, 255, 0), 3)
+                    # cv2.circle(frame, (global_cX, global_cY), 5, (0, 255, 0), -1)
                     #cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+            if max_cnt is not None:
+                max_M = cv2.moments(max_cnt)
+
+                max_local_cX = int(max_M["m10"] / max_M["m00"])
+                max_local_cY = int(max_M["m01"] / max_M["m00"])
+
+                x, y, w, h = cv2.boundingRect(max_cnt)
+                global_x = x + x1
+                global_y = y + y1
+
+                max_global_cX = max_local_cX + x1
+                max_global_cY = max_local_cY + y1
+
+
+
+
+
+                cv2.rectangle(frame, (global_x, global_y), (global_x + w, global_y + h), (0, 255, 0), 3)
+                cv2.circle(frame, (max_global_cX, max_global_cY), 5, (0, 255, 0), -1)
+            # cv2.rectangle(frame, (global_x, global_y), (global_x + w, global_y + h), (0, 255, 0), 3)
+            # cv2.circle(frame, (global_cX, global_cY), 5, (0, 255, 0), -1)
+
+
 
             result_frame = cv2.bitwise_and(frame, frame, mask=full_mask_frame)
-            cv2.rectangle(result_frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-            cv2.imshow('hsv_frame', result_frame)
-            # frame_count += 1q
+            cv2.imshow('hsv_frame', frame)
+            # frame_count += 1
 
 
 
